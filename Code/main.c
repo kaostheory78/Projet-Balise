07254c959a03f80cfb98e79e19c75d6b0e46e9dc
Quentin Mercier <qmercier@hotmail.fr>
2015-03-08 22:31:15 +0100

    pas grand chose

---
+++ Code/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Quentin
+ *
+ * Created on 6 mars 2015, 08:40
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
